f3ff100d404bde53c92576e0e0d2dd4f18d31ce7
JunPark <phjun0324@gmail.com>
2022-02-19 00:35:32 +0900

    메인페이지 작업중

--- youtube/src/pages/Main.js
+++ youtube/src/pages/Main.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import { Grid } from '../elements';
+import Category from '../components/Category';
+import PostCard from '../components/PostCard';
+
 const Main = () => {
-  return <>test</>;
+  return (
+    <>
+      <Category />
+      <Grid bg="#123456" width="100vw" height="100vh">
+        <PostCard></PostCard>
+      </Grid>
+    </>
+  );
 };
 
 export default Main;
