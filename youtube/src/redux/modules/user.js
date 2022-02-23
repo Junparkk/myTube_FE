3af4bca6779f06da60be765c129f7758930e0854
yun2021 <77830226+yun2021@users.noreply.github.com>
2022-02-23 20:30:54 +0900

    헤더 is_login 성공

--- youtube/src/redux/modules/user.js
+++ youtube/src/redux/modules/user.js
@@ -40,11 +40,13 @@ const loginDB = (userid, pwd) => {
             channelname: res.data.channelname,
           })
         );
-        history.replace('/');
+        // history.replace('/');
+        window.location.href = "/"
       })
+
       .catch((err) => {
-        console.log(err);
-        alert(err.res.data.errorMessage);
+        console.log("errorMessags", err.response.data);
+        alert(err.response.data);
       });
   };
 };
